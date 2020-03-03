24e98c247daf8b008bc5c4819f8f0b9eb21edb19
Dmitriy Amelchenko <d.a.amelchenko@outlook.com>
2020-03-03 20:06:56 +0300

    for in templates

--- e_shop/home/views.py
+++ e_shop/home/views.py
@@ -7,31 +7,37 @@ def home_page(request):
             {
                 "name": "Young Woman Wearing Dress 1",
                 "price": 120.00,
+                "sale": 50.00,
                 "image": "product-1.jpg"
             },
             {
                 "name": "Young Woman Wearing Dress 2",
                 "price": 110.00,
+                "sale": 20.00,
                 "image": "product-2.jpg"
             },
             {
                 "name": "Young Woman Wearing Dress 3",
                 "price": 80.00,
+                "sale": 30.00,
                 "image": "product-3.jpg"
             },
             {
                 "name": "Young Woman Wearing Dress 4",
                 "price": 800.00,
+                "sale": 60.00,
                 "image": "product-4.jpg"
             },
             {
                 "name": "Young Woman Wearing Dress 5",
                 "price": 200.00,
+                "sale": 50.00,
                 "image": "product-5.jpg"
             },
             {
                 "name": "Young Woman Wearing Dress 6",
                 "price": 150.00,
+                "sale": 40.00,
                 "image": "product-6.jpg"
             }
         ]
